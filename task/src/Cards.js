cf749741c13c4b7fa5fc7add60b860e88b0849da
Shikha Goyal <23shikha.s@gmail.com>
2021-06-14 20:39:21 +0530

    scroll added

--- task/src/Cards.js
+++ task/src/Cards.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios'
+import axios from "axios";
 import "./cards.css";
 
 export default function Cards() {
@@ -15,20 +15,33 @@ export default function Cards() {
       .get(`https://api.instantwebtools.net/v1/passenger?page=${page}&size=8`)
       .then((res) => res.data)
       .then((data) => setUsers([...users, ...data.data]));
-  }
+  };
+
+  const scrollToEnd = () => {
+    setPage(page + 1);
+  };
+
+  window.onscroll = function () {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      scrollToEnd();
+    }
+  };
 
   return (
-      <>
-    <div className="lingoq-cards">
-      {users.length > 0
-        ? users.map((data) => (
-            <div className="card one">
-              <h1>Name: {data.name}</h1>
-              <h1>Trips: {data.trips}</h1>
-            </div>
-          ))
-        : "Data field is empty"}
-    </div>
+    <>
+      <div className="lingoq-cards">
+        {users.length > 0
+          ? users.map((data, id) => (
+              <div key={data.id} className="card one">
+                <h1>Name: {data.name}</h1>
+                <h1>Trips: {data.trips}</h1>
+              </div>
+            ))
+          : "Data field is empty"}
+      </div>
     </>
   );
 }
